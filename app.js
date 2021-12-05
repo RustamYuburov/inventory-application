c6c11142fc07e0a7b158ffdf6a2d6cec3c5fa624
Rustam Yuburov <rt138328@gmail.com>
2021-12-05 20:29:14 +0600

    Finished project version 1.0

--- app.js
+++ app.js
@@ -1,34 +1,57 @@
 var createError = require('http-errors');
 var express = require('express');
+var favicon = require('serve-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
+app.use(compression()); //Compress all routes
+app.use(helmet());
+app.use(favicon(path.join(__dirname, 'public', 'images', 'site-favicon.ico')));
+
+const mongoose = require('mongoose');
+const dev_db_url =
+  'mongodb+srv://magma:inventory1234app@clusterinventory.45gwj.mongodb.net/inventory_application?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(expressLayouts);
+app.set('views', [
+  path.join(__dirname, 'views'),
+  path.join(__dirname, 'views/genre/'),
+  path.join(__dirname, 'views/game/'),
+  path.join(__dirname, 'views/developer/'),
+  path.join(__dirname, 'views/partials/'),
+]);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
